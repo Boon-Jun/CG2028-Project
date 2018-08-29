e324ca88002b483d05946b90144718574ef2edfe
kianmeng <35647345+kianmengtan@users.noreply.github.com>
2018-08-29 16:07:42 +0800

    pdm.s new

--- CG2028Asmt1S1AY201819/src/pdm.s
+++ CG2028Asmt1S1AY201819/src/pdm.s
@@ -6,25 +6,22 @@
  	.thumb_func
 pdm:
 @ write the code for pdm(CM,M,index) here
-	PUSH {R4-R6};
+	PUSH {R3-R4};
 	MOV R4, 4 @length of word
 	MUL R3, R1, R2 @Calculate initial offset(without considering word length)
-	MUL R3, R4 @offset when considering word length of 4 bytes
+	MUL R3, R4 @consider word length
 	ADD R0, R3  @apply offset
+	MUL R2, R4 @offset for index(accounting for word length)
+	ADD R4, R0, R2 @mem loc for numerator
+	LDR R2, [R4] @load numerator into R2
 	MOV R4, 0 @initialize sum to 0
-	MOV R5, 0 @initialize loop count to 0
 loop:
-	LDR R3, [R0], 4 @Load values with post-indexed addressing
-	CMP R5, R2 @compares loop count and index
-	BNE loop2
-storeBranch:
-	MOVW R6, 10000
-	MUL R6, R3
-loop2:
+	LDR R3, [R0], 4
 	ADD R4, R3  @Calculating the denominator of the PD function
-	ADD R5, 1 @R5 stores number of loops
-	CMP R5, R1 @compares loop count and M
-	BNE loop
-	SDIV R0, R6, R4 @numerator/denominator
-	POP {R4-R6};
-	BX	LR
+	SUBS R1, 1
+	BGT loop
+	MOVW R1, 10000
+	MUL R2, R1@multiply numerator by 10000
+	SDIV R0, R2, R4 @numerator/denominator
+	POP {R3-R4};
+	BX LR
