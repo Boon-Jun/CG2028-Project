ab7abf60baa8e26d51e361a8d438c52981eaadc2
ladderinc <boon-jun1996@hotmail.com>
2018-09-15 20:06:56 +0800

    Added test function

--- CG2028Asmt1S1AY201819/src/main.c
+++ CG2028Asmt1S1AY201819/src/main.c
@@ -7,19 +7,30 @@ extern int pdm(int* base, int ncol, int index);
 
 // Dimension of confusion matrix, i.e. MxM square matrix
 // Note: pdm() must work for different values of M, e.g. M=10
-#define M 3
+#define M 9
+
+float pdmTest(int *CM, int size, int index){
+	int x;
+	int store = 0, store2 = 0;
+	for(x = 0; x < size; x++){
+		if(x == index) store = *(CM + size * (index) + x);
+		store2 += *(CM + size * (index) + x);
+		//printf("%d ", *(CM + size * (index) + x));
+	}
+	//printf("\n");
+	return store/(float)store2;
+}
 
 int main(void)
 {
 	// Variable definitions
 	int index;
 	// Note: different initialisation list is needed for different M
-	int CM[M][M]={{60,2,3},{11,47,7},{27,14,24}};
-
+	int CM[M][M]= {42, 8, 169, 179, 10, 240, 223, 248, 140, 55, 221, 11, 45, 2, 40, 49, 33, 229, 105, 180, 181, 1, 25, 73, 69, 184, 42, 111, 190, 53, 171, 27, 103, 245, 6, 103, 107, 216, 124, 114, 140, 234, 21, 41, 106, 13, 145, 127, 174, 110, 89, 203, 214, 95, 35, 153, 119, 82, 150, 186, 24, 155, 86, 18, 149, 99, 102, 244, 221, 126, 43, 47, 232, 68, 221, 31, 198, 114, 201, 168, 195};
 	// PDm: Call assembly language function pdm() for each class m
 	// note: index = m - 1
-	for ( index=0; index<M; index++ )
-		printf("%f \n", pdm((int*)CM, M, index)/10000.0);
+	for (index=0; index<M; index++ )
+		printf("%f %f\n", pdm((int*)CM, M, index)/10000.0, pdmTest(CM, M, index));
 
 	// Enter an infinite loop, just incrementing a counter
 	volatile static int loop = 0;
@@ -27,3 +38,6 @@ int main(void)
 		loop++;
 	}
 }
+
+
+
