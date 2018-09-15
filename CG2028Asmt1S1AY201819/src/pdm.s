cd80ba3d2ab5ea0335e5af7561bcbdfc85b3f27d
kianmeng <35647345+kianmengtan@users.noreply.github.com>
2018-09-16 02:52:28 +0800

    reverted changes and added comments for some

--- CG2028Asmt1S1AY201819/src/pdm.s
+++ CG2028Asmt1S1AY201819/src/pdm.s
@@ -12,18 +12,17 @@ pdm:
 	MUL R3, R4 @consider word length 						0x00003413
 	ADD R0, R3  @apply offset 							0x00800003
 	MUL R2, R4 @offset for index(accounting for word length)			0x00002412
-	//ADD R4, R0, R2 @mem loc for numerator						0x00804002
-	//LDR R2, [R4] @load numerator into R2						0x04142000
-	ADD R2, R0 @load numerator into R2						0x00822000
-	LDR R2, [R2]
+	ADD R4, R0, R2 @mem loc for numerator						0x00804002
+	LDR R2, [R4] @load numerator into R2						0x04142000
+	//ADD R2, R0 @load numerator into R2						0x00822000
+	//LDR R2, [R2]
 	MOV R4, 0 @initialize sum to 0							0x03A04000
 loop:
-	LDR R3, [R0], 4									@0x04903004
-	ADD R4, R3  @Calculating the denominator of the PD function			0x00844003
-	//MOV R4, R3 @calculate the denominator of the PD function			
-	SUBS R1, 1									@0x02911001
-	BGT loop									@0xC8000010
-	MOVW R1, 10000
+	LDR R3, [R0], 4	@loads value in mem add of R0 to R3, then post-add		0x04903004
+	ADD R4, R3  @Calculating the denominator of the PD function			0x00844003			
+	SUBS R1, 1 @decrements R1(M), with flags set					0x02911001
+	BGT loop @branch back to loop when cond greater than is not met 		0xC8000010
+	MOVW R1, 10000 @assigns R1 with value 10000
 	MUL R2, R1  @multiply numerator by 10000					0x00002112	
 	SDIV R0, R2, R4 @numerator/denominator						
 	POP {R3-R4};
